c9299fb62a5220011b6b232144c327b281534bc3
39394930 <stefaniaandreiana2018@gmail.com>
2025-01-17 15:46:24 +0200

    Add files via upload

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -10,37 +10,19 @@ import java.rmi.RemoteException;
 import java.util.Base64;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
-public class ImageImplement extends UnicastRemoteObject implements ImageProcessor {
-
-    protected ImageImplement() throws RemoteException {
-        super();
-    }
-
-    // Implementarea metodei pentru prelucrarea imaginii
-    @Override
-    public void processImage(String base64Image) throws RemoteException {
-        try {
-            // Decodificăm imaginea din Base64
-            byte[] imageData = Base64.getDecoder().decode(base64Image);
-
-            // Salvăm imaginea ca fișier pe server
-            try (FileOutputStream fos = new FileOutputStream("received_image_part1.jpg")) {
-                fos.write(imageData);
-                System.out.println("Prima jumătate a imaginii a fost procesată și salvată!");
-            }
-        } catch (IOException e) {
-            System.err.println("Eroare la procesarea imaginii: " + e.getMessage());
-        }
-    }
-}
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Base64;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public interface ImageProcessor extends Remote {
     void processImage(String base64Image) throws RemoteException;
 }
 
-public class ImageImplement extends UnicastRemoteObject implements ImageProcessor {
+class ImageImplement extends UnicastRemoteObject implements ImageProcessor {
 
+    // Constructorul clasei care lansează excepțiile RemoteException
     protected ImageImplement() throws RemoteException {
         super();
     }
@@ -62,6 +44,7 @@ public class ImageImplement extends UnicastRemoteObject implements ImageProcesso
         }
     }
 }
+
 public class ImageServer {
 
     public static void main(String[] args) {
