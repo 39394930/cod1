6d5de7700f132e07d42740b88b4f4e3ae466cc0a
39394930 <stefaniaandreiana2018@gmail.com>
2025-01-17 16:39:48 +0200

    Add files via upload

--- JMSClient.java
+++ JMSClient.java
@@ -1,69 +1,140 @@
+package javalin;
+
 import jakarta.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import java.io.FileOutputStream;
-import java.util.Base64;
+import javax.naming.NamingException;
 import java.util.Properties;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Base64;
 
-public class JMSSubscriber {
+public class JMSClient {
 
-    public static Properties getProp(String ip, String port) {
-        Properties props = new Properties();
-        props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-        props.setProperty(Context.PROVIDER_URL, "tcp://" + ip + ":" + port);
-        return props;
-    }
+    protected static final String url = "tcp://localhost:61617";  // Broker URL
 
     public static void main(String[] args) {
-        Connection connection = null;
-        Session session = null;
+        String topicName = null;
+        Context jndiContext = null;
+        TopicConnectionFactory topicConnectionFactory = null;
+        TopicConnection topicConnection = null;
+        TopicSession topicSession = null;
+        Topic topic = null;
+        TopicSubscriber topicSubscriber = null;
+        TextListener topicListener = null;
+        InputStreamReader inputStreamReader = null;
+        char answer = '\0';
 
+        // Dacă nu există argumente, folosim valoarea implicită
+        if (args.length != 2) {
+            System.out.println("Imaginea se afla la adresa http://172.17.0.4 TOPIC:Topic_DAD");
+            System.exit(1);
+        }
+
+        topicName = args[1];  // Numele topicului
+        System.out.println("Topic name = " + topicName);
+
+        // Configurarea JNDI context + look-up factory & topic
         try {
-            // Configurarea conexiunii la broker-ul JMS
-            InitialContext jndiContext = new InitialContext(getProp(args[0], args[1]));
-            ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
-            connection = connectionFactory.createConnection();
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Properties props = new Properties();
+            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+            props.setProperty(Context.PROVIDER_URL, args[0]);  // URL-ul brokerului
 
-            // Crearea topic-ului pentru a primi mesaje
-            Destination destination = session.createTopic("jms/topic/test");
-            MessageConsumer consumer = session.createConsumer(destination);
+            jndiContext = new InitialContext(props);
+            topicConnectionFactory = (TopicConnectionFactory) jndiContext.lookup("ConnectionFactory");
+            topic = (Topic) jndiContext.lookup(topicName);
+        } catch (NamingException ne) {
+            ne.printStackTrace();
+            System.exit(2);
+        }
 
-            // Pornirea conexiunii JMS
-            connection.start();
+        // Conectarea la broker și abonarea la topic
+        try {
+            topicConnection = topicConnectionFactory.createTopicConnection();
+            topicSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            topicSubscriber = topicSession.createSubscriber(topic);
 
-            // Citirea mesajului care conține imaginea în Base64
-            Message message = consumer.receive();  // Blochează până când mesajul este primit
+            // Setarea listener-ului pentru mesaje
+            topicListener = new TextListener();
+            topicSubscriber.setMessageListener(topicListener);
 
-            if (message instanceof TextMessage) {
-                String base64Image = ((TextMessage) message).getText();
-                System.out.println("Imagine primită în Base64: " + base64Image);
+            // Pornirea conexiunii
+            topicConnection.start();
 
-                // Decodificarea string-ului Base64 în byte array
-                byte[] imageBytes = Base64.getDecoder().decode(base64Image);
-
-                // Salvarea imaginii într-un fișier
-                try (FileOutputStream fos = new FileOutputStream("imagine_receptionata.jpg")) {
-                    fos.write(imageBytes);
-                    System.out.println("Imaginea a fost salvată cu succes.");
-                } catch (Exception e) {
+            System.out.println("Se asteapta primirea imaginii... Pentru a inchide programul, apasa q + CR/LF");
+            inputStreamReader = new InputStreamReader(System.in);
+            while (!(answer == 'q')) {
+                try {
+                    answer = (char) inputStreamReader.read();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        } catch (JMSException jmse) {
+            jmse.printStackTrace();
+        } finally {
+            if (topicConnection != null) {
+                try {
+                    topicConnection.close();
+                } catch (JMSException e) {
                     e.printStackTrace();
                 }
-            } else {
-                System.out.println("Mesajul primit nu este de tipul TextMessage.");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (JMSException e) {
-                e.printStackTrace();
             }
         }
     }
-}
 
+    // Listener care procesează mesajele primite
+    static class TextListener implements MessageListener {
+        @Override
+        public void onMessage(Message message) {
+            TextMessage msg = null;
+
+            try {
+                if (message instanceof TextMessage) {
+                    msg = (TextMessage) message;
+                    String jsonMessage = msg.getText();
+
+                    // Parsarea mesajului JSON fără org.json
+                    // Extragem manual valorile din JSON
+                    String imageBase64 = extractJsonValue(jsonMessage, "image");
+                    String fileName = extractJsonValue(jsonMessage, "fileName");
+
+                    // Decodarea imaginii din Base64
+                    byte[] imageBytes = Base64.getDecoder().decode(imageBase64);
+
+                    // Salvarea imaginii într-un fișier
+                    try (FileOutputStream fos = new FileOutputStream(new File(fileName))) {
+                        fos.write(imageBytes);
+                        System.out.println("Imaginea a fost salvată în fișier");
+                    }
+
+                } else {
+                    System.out.println("Mesajul nu este de tip TextMessage!");
+                }
+            } catch (JMSException jmse) {
+                jmse.printStackTrace();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        }
+
+        // Funcție pentru a extrage valoarea unui câmp JSON manual
+        private String extractJsonValue(String jsonMessage, String key) {
+            int keyIndex = jsonMessage.indexOf("\"" + key + "\":");
+            if (keyIndex == -1) {
+                return null;
+            }
+
+            int startIndex = jsonMessage.indexOf("\"", keyIndex + key.length() + 3) + 1;
+            int endIndex = jsonMessage.indexOf("\"", startIndex);
+
+            if (startIndex == -1 || endIndex == -1) {
+                return null;
+            }
+
+            return jsonMessage.substring(startIndex, endIndex);
+        }
+    }
+}
