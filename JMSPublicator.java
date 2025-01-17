ab94c851a0e467f88f43e5e4ad59b3af8aa65973
39394930 <stefaniaandreiana2018@gmail.com>
2025-01-17 17:06:05 +0200

    Add files via upload

--- JMSPublicator.java
+++ JMSPublicator.java
@@ -1,79 +1,71 @@
 package apachetomeejms;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Properties;
 import jakarta.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.io.*;
+import java.util.Base64;
+import java.util.Properties;
 
-public class MessageProducerClient_JakartaTomEE {
-
-    public static Properties getProp(String ip, String port) {
-        Properties props = new Properties();
-        props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-        props.setProperty(Context.PROVIDER_URL, "tcp://" + ip + ":" + port);
-        return props;
-    }
-
-    public static void main(String args[]) {
-
-        Connection connection = null;
+public class JMSPublicator extends JavalinServer {
+	 public static Properties getProp(String ip, String port) {
+		 Properties props = new Properties();
+		 props.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+		 "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+		 //props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61617");
+	         props.setProperty(Context.PROVIDER_URL, "tcp://"+ip+":"+port);
+		 return props;
+	 }
+    public static void main(String[] args) {
+        // Configurăm și trimitem imaginea la brokerul ActiveMQ
         try {
-            InitialContext jndiContext = new InitialContext(getProp(args[0], args[1]));
+            // Creăm contextul JNDI și obținem conexiunea
+            InitialContext jndiContext = new InitialContext(getProp(args[0],args[1]));
             ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
-            connection = connectionFactory.createConnection();
-            connection.setClientID("durable");
+            Connection connection = connectionFactory.createConnection();
+            connection.start();  // Pornim conexiunea
+
+            // Creăm sesiunea și topic-ul pentru publicare
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createTopic("jms/topic/test");
+            Destination destination = session.createTopic("jms/topic/test"); // Topicul unde trimitem mesajele
+
+            // Creăm producer-ul pentru a trimite mesaje pe topic
             MessageProducer producer = session.createProducer(destination);
 
-            // Citirea fișierului imagine (exemplu: "image.jpg")
-            File imageFile = new File("image.jpg");
-            FileInputStream fis = new FileInputStream(imageFile);
-            byte[] imageBytes = new byte[(int) imageFile.length()];
-            fis.read(imageBytes);
-            fis.close();
-
-            // Crearea unui BytesMessage și trimiterea imaginii
-            BytesMessage msg = session.createBytesMessage();
-            msg.writeBytes(imageBytes);
-            msg.setStringProperty("fileName", imageFile.getName()); // Opțional: setăm numele fișierului în proprietăți
-
-            // Citirea mesajelor din consolă (poți trimite alte mesaje text dacă vrei)
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-            while (true) {
-                System.out.println("Enter Message to Topic or Press 'Q' for Close this Session");
-                String input = reader.readLine();
-                if ("Q".equalsIgnoreCase(input.trim())) {
-                    break;
+            // Citim fișierul imagine și îl codificăm în Base64
+            File imageFile = new File("C:\\Users\\Stefania\\Documents\\Imagine.bmp");  // Înlocuiește cu calea fișierului tău
+            if (imageFile.exists()) {
+                byte[] imageData = new byte[(int) imageFile.length()];
+                try (FileInputStream fis = new FileInputStream(imageFile)) {
+                    fis.read(imageData);
                 }
-                // Dacă utilizatorul nu vrea să trimită o imagine, poate trimite un mesaj text
-                if (!input.trim().isEmpty()) {
-                    TextMessage textMsg = session.createTextMessage(input);
-                    producer.send(textMsg);
-                }
+
+                // Codificăm imaginea în Base64
+                String imageBase64 = Base64.getEncoder().encodeToString(imageData);
+
+                // Construim mesajul JSON cu imaginea codificată și numele fișierului
+                String jsonMessage = "{"
+                        + "\"image\":\"" + imageBase64 + "\","
+                        + "\"fileName\":\"" + imageFile.getName() + "\""
+                        + "}";
+
+                // Creăm mesajul JMS de tip TextMessage
+                TextMessage message = session.createTextMessage(jsonMessage);
+
+                // Trimiterea mesajului către topic
+                producer.send(message);
+                System.out.println("Imaginea a fost trimisă pe topic!");
+
+            } else {
+                System.out.println("Fișierul imagine nu a fost găsit.");
             }
 
-            // Trimiterea imaginii
-            producer.send(msg);
-            System.out.println("Image sent successfully!");
+            // Închidem conexiunea
+            connection.close();
 
-        } catch (JMSException | IOException | NamingException e) {
+        } catch (JMSException | NamingException | IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (JMSException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
